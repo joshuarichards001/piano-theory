9ea9d77af118a97abed8b253a5ffc17cd6ec6c0c
josh richards <joshuarichards001@gmail.com>
2024-02-28 17:45:03 +1300

    Testing new piano sizing

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,7 @@ import Piano from "./Piano";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-between">
       <h1 className="text-xl mb-10">Piano Scales</h1>
       <Piano />
     </div>
