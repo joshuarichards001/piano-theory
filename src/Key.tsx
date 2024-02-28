a5902f38a2bed6e1bfb35e6e01cfbbd2419c8e03
josh richards <joshuarichards001@gmail.com>
2024-02-28 17:50:48 +1300

    Adding active property to keys

--- src/Key.tsx
+++ src/Key.tsx
@@ -8,12 +8,12 @@ export default function Key({ type, note }: Props) {
   if (type === "white") {
     return (
       <div
-        className={`grow-[3] h-24 bg-white border-2 border-black last:border-r-0 ${
+        className={`grow-[3] h-24 bg-white active:bg-gray-300 border-2 border-black last:border-r-0 ${
           (note === "f" || note === "c") && "border-l-0"
         }`}
       />
     );
   } else {
-    return <div className="grow-[2] h-14 bg-black z-10 -mx-[3%]" />;
+    return <div className="grow-[2] h-14 bg-black active:bg-gray-700 z-10 -mx-[3%]" />;
   }
 }
