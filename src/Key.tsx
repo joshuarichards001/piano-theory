1596a9db09adee29f6473619955bcb8a2057d391
josh richards <joshuarichards001@gmail.com>
2024-02-28 20:27:19 +1300

    Add sound to keyboard

--- src/Key.tsx
+++ src/Key.tsx
@@ -4,18 +4,27 @@ type Props = {
   fileName: string;
 };
 
-export default function Key({ type, note }: Props) {
+export default function Key({ type, note, fileName }: Props) {
+  const onClick = () => {
+    const audio = new Audio(`/notes/${fileName}`);
+    audio.play();
+  };
+
   if (type === "white") {
     return (
       <div
-        className={`grow-[3] h-24 bg-white active:bg-neutral-400 border-2 border-black last:border-r-0 ${
-          (note === "f" || note === "c") && "border-l-0"
-        }`}
+        onClick={onClick}
+        className={`grow-[3] h-24 bg-white active:bg-neutral-400 border-2 border-black ${
+          (note === "E" || note === "B") && "border-r-0"
+        } last:border-r-2`}
       />
     );
   } else {
     return (
-      <div className="grow-[2] h-14 bg-black active:bg-neutral-600 z-10 -mx-[3%]" />
+      <div
+        onClick={onClick}
+        className="grow-[2] h-14 bg-black active:bg-neutral-600 z-10 -mx-[3%]"
+      />
     );
   }
 }
--- src/Piano.tsx
+++ src/Piano.tsx
@@ -2,31 +2,31 @@ import Key from "./Key";
 
 export default function Piano() {
   return (
-    <div className="flex w-full max-w-4xl">
-      <Key type="white" note="c" fileName="c.mp3" />
-      <Key type="black" note="db" fileName="db.mp3" />
-      <Key type="white" note="d" fileName="d.mp3" />
-      <Key type="black" note="eb" fileName="eb.mp3" />
-      <Key type="white" note="e" fileName="e.mp3" />
-      <Key type="white" note="f" fileName="f.mp3" />
-      <Key type="black" note="gb" fileName="gb.mp3" />
-      <Key type="white" note="g" fileName="g.mp3" />
-      <Key type="black" note="ab" fileName="ab.mp3" />
-      <Key type="white" note="a" fileName="a.mp3" />
-      <Key type="black" note="bb" fileName="bb.mp3" />
-      <Key type="white" note="b" fileName="b.mp3" />
-      <Key type="white" note="c" fileName="c.mp3" />
-      <Key type="black" note="db" fileName="db.mp3" />
-      <Key type="white" note="d" fileName="d.mp3" />
-      <Key type="black" note="eb" fileName="eb.mp3" />
-      <Key type="white" note="e" fileName="e.mp3" />
-      <Key type="white" note="f" fileName="f.mp3" />
-      <Key type="black" note="gb" fileName="gb.mp3" />
-      <Key type="white" note="g" fileName="g.mp3" />
-      <Key type="black" note="ab" fileName="ab.mp3" />
-      <Key type="white" note="a" fileName="a.mp3" />
-      <Key type="black" note="bb" fileName="bb.mp3" />
-      <Key type="white" note="b" fileName="b.mp3" />
+    <div className="flex w-full max-w-xl">
+      <Key type="white" note="C" fileName="C3.mp3" />
+      <Key type="black" note="Db" fileName="Db3.mp3" />
+      <Key type="white" note="D" fileName="D3.mp3" />
+      <Key type="black" note="Eb" fileName="Eb3.mp3" />
+      <Key type="white" note="E" fileName="E3.mp3" />
+      <Key type="white" note="F" fileName="F3.mp3" />
+      <Key type="black" note="Gb" fileName="Gb3.mp3" />
+      <Key type="white" note="G" fileName="G3.mp3" />
+      <Key type="black" note="Ab" fileName="Ab3.mp3" />
+      <Key type="white" note="A" fileName="A3.mp3" />
+      <Key type="black" note="Bb" fileName="Bb3.mp3" />
+      <Key type="white" note="B" fileName="B3.mp3" />
+      <Key type="white" note="C" fileName="C4.mp3" />
+      <Key type="black" note="Db" fileName="Db4.mp3" />
+      <Key type="white" note="D" fileName="D4.mp3" />
+      <Key type="black" note="Eb" fileName="Eb4.mp3" />
+      <Key type="white" note="E" fileName="E4.mp3" />
+      <Key type="white" note="F" fileName="F4.mp3" />
+      <Key type="black" note="Gb" fileName="Gb4.mp3" />
+      <Key type="white" note="G" fileName="G4.mp3" />
+      <Key type="black" note="Ab" fileName="Ab4.mp3" />
+      <Key type="white" note="A" fileName="A4.mp3" />
+      <Key type="black" note="Bb" fileName="Bb4.mp3" />
+      <Key type="white" note="B" fileName="B4.mp3" />
     </div>
   )
 }
