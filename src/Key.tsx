2ef9bb3ebf6738347a1b04e8e972a38b73845fd1
josh richards <joshuarichards001@gmail.com>
2024-02-28 20:55:27 +1300

    Potential fix for key sound latency

--- src/Key.tsx
+++ src/Key.tsx
@@ -1,26 +1,19 @@
-import { useEffect, useState } from "react";
-
 type Props = {
   note: string;
   fileName: string;
 };
 
 export default function Key({ note, fileName }: Props) {
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-
-  useEffect(() => {
-    const audioObj = new Audio(`/notes/${fileName}`);
-    setAudio(audioObj);
-  }, [fileName]);
-
   const onClick = () => {
+    const audio = new Audio(`/notes/${fileName}`);
     audio?.play();
   };
 
   if (!note.includes("b")) {
     return (
       <div
-        onClick={onClick}
+        onMouseDown={onClick}
+        onTouchStart={onClick}
         className={`grow-[3] h-24 bg-white active:bg-neutral-400 border-2 border-black ${
           (note === "E" || note === "B") && "border-r-0"
         } last:border-r-2`}
@@ -29,7 +22,8 @@ export default function Key({ note, fileName }: Props) {
   } else {
     return (
       <div
-        onClick={onClick}
+        onMouseDown={onClick}
+        onTouchStart={onClick}
         className="grow-[2] h-14 bg-black active:bg-neutral-600 z-10 -mx-[3%]"
       />
     );
