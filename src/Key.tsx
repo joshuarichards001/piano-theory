3f4b74bde9309d26885577dd61aad2944b1c3960
josh richards <joshuarichards001@gmail.com>
2024-02-28 17:38:15 +1300

    Change key height

--- src/Key.tsx
+++ src/Key.tsx
@@ -8,12 +8,12 @@ export default function Key({ type, note }: Props) {
   if (type === "white") {
     return (
       <div
-        className={`grow-[2] h-72 bg-white border-2 border-black ${
+        className={`grow-[2] h-36 bg-white border-2 border-black ${
           (note === "f" || note === "c") && "border-l-0"
         }`}
       />
     );
   } else {
-    return <div className="grow h-40 bg-black z-10 -mx-[2%]" />;
+    return <div className="grow h-24 bg-black z-10 -mx-[2%]" />;
   }
 }
