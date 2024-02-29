1e061668a77ef20f5daff95027274f37c8e65b12
Josh Richards <joshuarichards001@gmail.com>
2024-02-29 23:28:59 +0000

    Change color based on quiz type and add to gitignore for codesandbox

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -15,6 +15,20 @@ export default function Quiz({ quizType }: Props) {
   const [score, setScore] = useState(0);
   const [done, setDone] = useState(false);
 
+  const noteColour = () => {
+    if (quizType === "major-scale") {
+      return "bg-success";
+    } else if (quizType === "natural-minor-scale") {
+      return "bg-warning";
+    } else if (quizType === "major-7th-chord") {
+      return "bg-primary";
+    } else if (quizType === "minor-7th-chord") {
+      return "bg-secondary";
+    } else if (quizType === "dominant-7th-chord") {
+      return "bg-accent";
+    }
+  };
+
   useEffect(() => {
     setQuestion(0);
     setScore(0);
@@ -37,7 +51,7 @@ export default function Quiz({ quizType }: Props) {
           <div className="p-6">
             <div className="flex justify-between mb-4">
               <div className="flex items-end gap-2">
-                <h3 className="text-4xl bg-primary rounded-lg px-1">
+                <h3 className={`text-4xl ${noteColour()} rounded-lg px-1`}>
                   {startNote.replace("3", "")}{" "}
                 </h3>
                 <h3 className="text-2xl">{quizType.replaceAll("-", " ")}</h3>
