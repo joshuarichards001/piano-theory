5e9a537ec3b102776937ae95543559865d68b337
josh richards <joshuarichards001@gmail.com>
2024-03-01 11:31:44 +1300

    Finished quiz styling

--- src/components/QuizComplete.tsx
+++ src/components/QuizComplete.tsx
@@ -6,22 +6,22 @@ type Props = {
 export default function QuizComplete({ score, numberOfQuestions }: Props) {
   const quizFeedback = () => {
     if (score === 12) {
-      return <p>Perfect score! Great job!</p>;
+      return "Perfect score! Great job!";
     } else if (score >= 9) {
-      return <p>Great job! You're getting there!</p>;
+      return "Great job! You're getting there!";
     } else if (score >= 6) {
-      return <p>Not bad! Keep practicing!</p>;
+      return "Not bad! Keep practicing!";
     } else {
-      return <p>Keep practicing! You'll get there!</p>;
+      return "Keep practicing! You'll get there!";
     }
   };
 
   return (
-    <div>
-      <p>{quizFeedback()}</p>
-      <div>
+    <div className="p-6 mb-16">
+      <p className="text-2xl mb-4">{quizFeedback()}</p>
+      <p className="text-xl">
         You scored {score} out of {numberOfQuestions}.
-      </div>
+      </p>
     </div>
   );
 }
