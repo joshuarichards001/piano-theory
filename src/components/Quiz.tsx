52778e914e2ae24cf2b897de8a2ee2873cd0ed7a
Josh Richards <joshuarichards001@gmail.com>
2024-03-03 22:41:52 +0000

    Add key counter for quiz. change quiz styles

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { NOTES } from "../constants";
 import { createQuiz, getQuizBackgroundColour } from "../functions";
 import Piano from "./Piano";
 import QuizComplete from "./QuizComplete";
+import { useAppSelector } from "../redux/hooks";
 
 type Props = {
   quizType: QuizType;
 };
 
 export default function Quiz({ quizType }: Props) {
-  const quiz = createQuiz(quizType);
+  const quiz = useMemo(() => createQuiz(quizType), [quizType]);
   const [questionNumber, setQuestionNumber] = useState(0);
-  const startNote = NOTES[quiz[questionNumber][0]];
+  const question = quiz[questionNumber];
+  const startNote = NOTES[question[0]];
+  const pressedKeys = useAppSelector((state) => state.pressedKeys);
+
   const [score, setScore] = useState(0);
   const [done, setDone] = useState(false);
 
@@ -38,18 +42,23 @@ export default function Quiz({ quizType }: Props) {
             <div className="flex justify-between mb-4">
               <div className="flex items-center gap-2">
                 <h3
-                  className={`text-5xl ${getQuizBackgroundColour(
+                  className={`text-4xl ${getQuizBackgroundColour(
                     quizType,
                   )} rounded-lg px-1`}
                 >
                   {startNote.replace("3", "")}{" "}
                 </h3>
                 <div>
-                  <h3 className="text-2xl">{quizType.replaceAll("-", " ")}</h3>
-                  <p className="text-xs text-gray-500">1/13 Keys Pressed</p>
+                  <h3 className="text-2xl capitalize">
+                    {quizType.replaceAll("-", " ")}
+                  </h3>
+                  <p className="text-xs text-gray-500">
+                    {pressedKeys.filter((key) => question.includes(key)).length}
+                    /{question.length} Keys Pressed
+                  </p>
                 </div>
               </div>
-                <h3 className="text-4xl">
+              <h3 className="text-2xl">
                 {questionNumber + 1}/{quiz.length}
               </h3>
             </div>
