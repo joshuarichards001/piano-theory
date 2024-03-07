7d7f4dd6a16b2ed12ba099e0394efeca2505758e
josh richards <joshuarichards001@gmail.com>
2024-03-07 20:12:43 +1300

    Install modal and pwa icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,30 @@
+import { IonIcon } from "@ionic/react";
+import { downloadOutline } from "ionicons/icons";
 import { Outlet } from "react-router-dom";
+import InstallModal from "./InstallModal";
 
 export default function Home() {
   return (
     <div className="h-full flex flex-col max-w-xl mx-auto">
       <div className="p-6">
-        <h1 className="text-4xl font-bold mb-6">Piano Theory</h1>
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-4xl font-bold">Piano Theory</h1>
+          <button
+            onClick={() => {
+              (
+                document.getElementById("install-modal") as HTMLFormElement
+              ).showModal();
+            }}
+          >
+            <IonIcon icon={downloadOutline} className="h-6 w-6" color="white" />
+          </button>
+        </div>
         <p className="text-gray-500">
-        Dive into the world of music theory. Pick a topic, and see how quickly and accurately you can hit the right notes!
+          Dive into the world of music theory. Pick a topic, and see how quickly
+          and accurately you can hit the right notes!
         </p>
       </div>
+      <InstallModal />
       <Outlet />
     </div>
   );
---
+++ src/components/InstallModal.tsx
@@ -0,0 +1,90 @@
+import { IonIcon } from "@ionic/react";
+import {
+  addCircle,
+  ellipsisVertical,
+  phonePortraitOutline,
+  shareOutline,
+} from "ionicons/icons";
+
+export default function Modal() {
+  const getOS = () => {
+    if (/android/i.test(navigator.userAgent)) {
+      return "Android";
+    } else if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+      return "iOS";
+    } else {
+      return "Desktop";
+    }
+  };
+
+  return (
+    <dialog id="install-modal" className="modal">
+      <div className="modal-box relative flex flex-col bg-background-highlight h-80">
+        <form method="dialog">
+          <button className="absolute right-6 top-6">✕</button>
+        </form>
+        <h2 className="text-2xl font-bold mb-2">Add to Home Screen</h2>
+        <p className="text-gray-500 mb-10">
+          This website has app functionality. Add it to your home screen to use
+          it in fullscreen.{" "}
+          {getOS() === "iOS"
+            ? "(make sure you're using Safari)"
+            : getOS() === "Android" && "(make sure you're using Chrome)"}
+          .
+        </p>
+
+        {getOS() !== "Desktop" ? (
+          <>
+            <div className="flex items-center justify-start mb-6">
+              {getOS() === "iOS" ? (
+                <>
+                  <IonIcon
+                    icon={shareOutline}
+                    className="h-6 w-6"
+                    color="white"
+                  />
+                  <p className="text-sm ml-3">
+                    1) Press the 'Share' button below on your browser
+                  </p>
+                </>
+              ) : getOS() === "Android" ? (
+                <>
+                  <IonIcon
+                    icon={ellipsisVertical}
+                    className="h-6 w-6"
+                    color="white"
+                  />
+                  <p className="text-sm ml-3">
+                    1) Press the 'Options' button at the top on your browser
+                  </p>
+                </>
+              ) : null}
+            </div>
+
+            <div className="flex items-center justify-start">
+              <IonIcon icon={addCircle} className="h-6 w-6" color="white" />
+              <p className="text-sm ml-3">2) Press 'Add to Home Screen'</p>
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center justify-start mb-6">
+            <div className="flex items-center justify-start">
+              <IonIcon
+                icon={phonePortraitOutline}
+                className="h-10 w-10"
+                color="white"
+              />
+              <p className="text-sm ml-3">
+                This is a mobile only feature, check out Album Listen List on
+                your phones browser!
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+}
