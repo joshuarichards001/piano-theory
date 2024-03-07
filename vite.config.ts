86897b330e03fcedf3c045f4db59c0aa0790ee7a
josh richards <joshuarichards001@gmail.com>
2024-03-07 20:37:19 +1300

    Update PWA Manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Piano Theory",
+        short_name: "Piano Theory",
+        description: "Learn piano theory",
+      },
+    }),
+  ],
 });
