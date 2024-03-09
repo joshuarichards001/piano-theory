05184594db99cd7a25226bc910465a1023047513
josh richards <joshuarichards001@gmail.com>
2024-03-09 21:26:56 +1300

    Refactor Landing component

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,36 +1,56 @@
-import { useNavigate } from "react-router-dom";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import { getQuizColour } from "../functions";
 
-export default function Landing() {
-  const navigate = useNavigate();
-
-  const QuizButton = ({ quizType }: { quizType: QuizType }) => (
+const QuizButton = ({
+  quizType,
+  navigate,
+}: {
+  quizType: QuizType;
+  navigate: NavigateFunction;
+}) => {
+  return (
     <button
       onClick={() => navigate(quizType)}
       className={`btn capitalize ${getQuizColour(quizType)}`}
     >
-      {quizType.replace(/-/g, " ").replace("chord", "").replace("scale", "")}
+      <p>
+        {quizType.replace(/-/g, " ").replace("chord", "").replace("scale", "")}
+      </p>
     </button>
   );
+};
+
+export default function Landing() {
+  const navigate = useNavigate();
 
   return (
     <div className="p-6">
       <div className="mb-10">
+        <h2 className="text-2xl mb-2 font-bold">Notes</h2>
+        <div className="flex flex-wrap gap-3">
+          <QuizButton quizType="notes" navigate={navigate} />
+        </div>
+      </div>
+
+      <div className="mb-10">
         <h2 className="text-2xl mb-2 font-bold">Scales</h2>
         <div className="flex flex-wrap gap-3">
-          <QuizButton quizType="major-scale" />
-          <QuizButton quizType="natural-minor-scale" />
+          <QuizButton quizType="major-scale" navigate={navigate} />
+          <QuizButton quizType="natural-minor-scale" navigate={navigate} />
         </div>
       </div>
 
       <div className="mb-10">
         <h2 className="text-2xl mb-2 font-bold">Chords</h2>
         <div className="flex flex-wrap gap-3">
-          <QuizButton quizType="major-7th-chord" />
-          <QuizButton quizType="minor-7th-chord" />
-          <QuizButton quizType="dominant-7th-chord" />
-          <QuizButton quizType="half-diminished-7th-chord" />
-          <QuizButton quizType="diminished-7th-chord" />
+          <QuizButton quizType="major-7th-chord" navigate={navigate} />
+          <QuizButton quizType="minor-7th-chord" navigate={navigate} />
+          <QuizButton quizType="dominant-7th-chord" navigate={navigate} />
+          <QuizButton
+            quizType="half-diminished-7th-chord"
+            navigate={navigate}
+          />
+          <QuizButton quizType="diminished-7th-chord" navigate={navigate} />
         </div>
       </div>
     </div>
