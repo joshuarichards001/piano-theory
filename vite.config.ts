8aa3cc5d87782d9b8ce27d20e72a42251dd6f477
josh richards <joshuarichards001@gmail.com>
2024-03-16 09:23:09 +1300

    Update app description and pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,20 @@ export default defineConfig({
         name: "Piano Theory",
         short_name: "Piano Theory",
         description: "Learn piano theory",
+        display: "fullscreen",
+        prefer_related_applications: false,
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
