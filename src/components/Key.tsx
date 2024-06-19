56a1b7072e8716ce246d6d6e92d45e23325336c5
josh richards <joshuarichards001@gmail.com>
2024-06-19 14:50:43 +1200

    Trying to fix safari audio problem?

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -21,7 +21,7 @@ function Key({ keyIndex, note, keyState }: Props) {
 
   useEffect(() => {
     const fetchData = async () => {
-      audioContext.current = new AudioContext();
+      audioContext.current = new (window.AudioContext || window.webkitAudioContext)();
       gainNode.current = audioContext.current.createGain();
       gainNode.current.connect(audioContext.current.destination);
 
