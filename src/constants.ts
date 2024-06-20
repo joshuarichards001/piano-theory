a1254eaf7f75949f2412a8f26f1270fc01439321
Josh Richards <joshuarichards001@gmail.com>
2024-06-20 23:17:30 +0000

    Updated blurb for notes, and scales

--- src/constants.ts
+++ src/constants.ts
@@ -19,7 +19,7 @@ export const QUIZ_TYPE_DATA_MAP = new Map<QuizType, QuizTypeData>([
     {
       keys: [0],
       name: "Notes",
-      info: "Notes start at C with every white note being a new letter. ♯'s and ♭'s are the black key higher or lower than the white key respectively.",
+      info: "White keys are notes from A to G, repeating in cycles. The piano below starts at C. Sharps (♯) and flats (♭) are the black key above or below the white key respectively. E♯ is the same as F, and C♭ is the same as B.",
       colour: "btn-primary",
     },
   ],
@@ -28,7 +28,7 @@ export const QUIZ_TYPE_DATA_MAP = new Map<QuizType, QuizTypeData>([
     {
       keys: [0, 2, 4, 5, 7, 9, 11, 12],
       name: "Major Scale",
-      info: "Major scales follow the formula of whole and half steps: W-W-H-W-W-W-H.",
+      info: "Major scales follow a formula of half and whole steps (half is one key over, whole is two keys over). Starting on any given note follow the pattern: W-W-H-W-W-W-H (e.g. D-E-F♯-G-A-B-C♯-D).",
       colour: "btn-primary",
     },
   ],
@@ -37,7 +37,7 @@ export const QUIZ_TYPE_DATA_MAP = new Map<QuizType, QuizTypeData>([
     {
       keys: [0, 2, 3, 5, 7, 8, 10, 12],
       name: "Natural Minor Scale",
-      info: "Natural Minor scales follow the formula of whole and half steps: W-H-W-W-H-W-W.",
+      info: "Natural Minor Scales follow a formula of half and whole steps (half is one key over, whole is two keys over). Starting on any given note follow the pattern: W-H-W-W-H-W-W (e.g. D-E-F-G-A-B♭-C-D).",
       colour: "btn-secondary",
     },
   ],
