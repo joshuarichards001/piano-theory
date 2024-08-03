3ff37268b4cbaf4ed5485809ff11b02dc68cb588
josh richards <joshuarichards001@gmail.com>
2024-08-03 16:30:49 +1200

    another try

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       },
       padding: {
         "header-safe-top": "max(env(safe-area-inset-top), 16px)",
-        "landing-safe-top": "max(env(safe-area-inset-top), 80px)",
+        "landing-safe-top": "max(env(safe-area-inset-top), 100px)",
         "safe-bottom": "env(safe-area-inset-bottom)",
       },
     },
