75b02b87d7ffa3957138827eb60f15fd515c1efe
josh richards <joshuarichards001@gmail.com>
2024-08-07 13:21:57 +1200

    Restructure useSilentAudio hook

--- src/useSilentAudio.ts
+++ src/useSilentAudio.ts
@@ -9,34 +9,28 @@ export function useSilentAudio() {
       return;
     }
 
-    let audio: HTMLAudioElement | null = null;
-
-    const setupAudio = () => {
-      hasRunRef.current = true;
-      audio = document.createElement("audio");
-      audio.setAttribute("x-webkit-airplay", "deny");
-      audio.preload = "auto";
-      audio.src = "1-minute-of-silence.mp3";
-      audio.loop = true;
-      audio.play();
-    };
+    hasRunRef.current = true;
+    const audio = document.createElement("audio");
+    audio.setAttribute("x-webkit-airplay", "deny");
+    audio.preload = "auto";
+    audio.src = "1-minute-of-silence.mp3";
+    audio.loop = true;
+    audio.play();
 
     const handleVisibilityChange = () => {
-      if (document.hidden && audio) {
+      if (document.hidden) {
         audio.pause();
-        audio.remove();
-        audio = null;
       } else {
-        setupAudio();
+        audio.play();
       }
     };
 
     document.addEventListener("visibilitychange", handleVisibilityChange);
 
-    // return () => {
-    //   document.removeEventListener("visibilitychange", handleVisibilityChange);
-    //   audio.pause();
-    //   audio.remove();
-    // };
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      audio.pause();
+      audio.remove();
+    };
   }, []);
 }
